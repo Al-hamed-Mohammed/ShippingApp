4410d70b5d3a27cc358942e8c030b10f2196b1bf
Al-hamed-Mohammed <mohd.vtt@gmail.com>
2021-09-06 14:03:19 +0530

    admintemplatewithLabelcrud
    admintemplatewithLabelcrud

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Shipping_Label_App.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,5 +13,6 @@ namespace Shipping_Label_App.Data
             : base(options)
         {
         }
+        public DbSet<Labels> Labels { get; set; }
     }
 }
