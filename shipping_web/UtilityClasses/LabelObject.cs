412283872329bf73235599d4fbc691fe02645b2a
Awez <awez.vtt@gmail.com>
2021-10-09 01:22:53 +0530

    Shipping rates using api and calculations part 1 pushed
    Shipping rates using api and calculations part 1 pushed

---
+++ shipping_web/UtilityClasses/LabelObject.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shipping_Label_App.UtilityClasses
+{
+    public class LabelObject
+    {
+        public origin_address origin_address { get; set; }
+        public destination_address destination_address { get; set; }
+        public string incoterms { get; set; }
+        public insurance insurance { get; set; }
+        public courier_selection courier_selection { get; set; }
+        public shipping_settings shipping_settings { get; set; }
+
+        public ICollection<parcels> parcels { get; set; }
+    }
+
+    public class origin_address
+    {
+        public string line_1 { get; set; }
+        public string line_2 { get; set; }
+        public string postal_code { get; set; }
+        public string state { get; set; }
+        public string city { get; set; }
+    }
+    public class destination_address
+    {
+        public string line_1 { get; set; }
+        public string line_2 { get; set; }
+        public string postal_code { get; set; }
+        public string state { get; set; }
+        public string city { get; set; }
+        public string country_alpha2 { get; set; }
+
+    }
+
+    public class insurance
+    {
+        public bool is_insured { get; set; }
+        public double insured_amount { get; set; }
+        public string insured_currency { get; set; }
+    }
+
+    public class courier_selection
+    {
+        public bool apply_shipping_rules { get; set; }
+    }
+
+    #region Shiping settings section
+    public class units
+    {
+        public string weight { get; set; }
+        public string dimensions { get; set; }
+    }
+    public class shipping_settings
+    {
+        public units units { get; set; }
+        public string output_currency { get; set; }
+    }
+    #endregion
+
+    #region Parcel section
+    public class box
+    {
+        public int length { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+    }
+
+    public class items
+    {
+        public int quantity { get; set; }
+        public box dimensions { get; set; }
+        public string category { get; set; }
+        public string description { get; set; }
+        public int actual_weight { get; set; }
+        public string declared_currency { get; set; }
+        public int declared_customs_value { get; set; }
+    }
+
+    public class parcels
+    {
+        public double total_actual_weight { get; set; }
+        public box box { get; set; }
+
+        public ICollection<items> items { get; set; }
+    }
+
+    #endregion
+}
---
+++ shipping_web/UtilityClasses/ShipingRatesModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shipping_Label_App.UtilityClasses
+{
+    public class ShipingRatesModel
+    {
+        public string status { get; set; }
+        public List<Rate> rates { get; set; }
+    }
+
+    public class Discount
+    {
+        public int amount { get; set; }
+        public object code { get; set; }
+        public object percentage { get; set; }
+        public object expires_at { get; set; }
+    }
+
+    public class Rate
+    {
+        public string courier_id { get; set; }
+        public string courier_name { get; set; }
+        public int min_delivery_time { get; set; }
+        public int max_delivery_time { get; set; }
+        public double value_for_money_rank { get; set; }
+        public double delivery_time_rank { get; set; }
+        public string currency { get; set; }
+        public double shipment_charge { get; set; }
+        public double fuel_surcharge { get; set; }
+        public double remote_area_surcharge { get; set; }
+        //public RemoteAreaSurcharges remote_area_surcharges { get; set; }
+        public double oversized_surcharge { get; set; }
+        public double additional_services_surcharge { get; set; }
+        public double residential_full_fee { get; set; }
+        public double residential_discounted_fee { get; set; }
+        public double shipment_charge_total { get; set; }
+        public double warehouse_handling_fee { get; set; }
+        public double insurance_fee { get; set; }
+        public double sales_tax { get; set; }
+        public double provincial_sales_tax { get; set; }
+        public double ddp_handling_fee { get; set; }
+        public double import_tax_charge { get; set; }
+        public double import_tax_non_chargeable { get; set; }
+        public double import_duty_charge { get; set; }
+        public double total_charge { get; set; }
+        public bool is_above_threshold { get; set; }
+        public string incoterms { get; set; }
+        public int estimated_import_tax { get; set; }
+        public int estimated_import_duty { get; set; }
+        public double minimum_pickup_fee { get; set; }
+        public string available_handover_options { get; set; }
+        public int tracking_rating { get; set; }
+        public double easyship_rating { get; set; }
+        public object courier_remarks { get; set; }
+        public string payment_recipient { get; set; }
+        public Discount discount { get; set; }
+        public string description { get; set; }
+        public string full_description { get; set; }
+    }
+}
